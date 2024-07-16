37ab52fd4996bedc85287af9ffee098c19dd6dc8
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-07-16 15:20:00 +0200

    change custom_user

--- src/sd_auth/models.py
+++ src/sd_auth/models.py
@@ -1,19 +1,23 @@
-from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None):  # **kwargs
-        if not email:
-            raise ValueError("Entrer un email")
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Entrer un nom d'utilisateur")
 
-        user = self.model(
-            email=self.normalize_email(email)
-        )
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        superuser = self.create_user(username, password, **extra_fields)
+        return superuser
 
     def modify_user(self, emalil, password=None):
         # pour conformité RGPD
@@ -23,30 +27,24 @@ class CustomUserManager(BaseUserManager):
         # autoriser la suppression pour conformité RGPD
         pass
 
-    def create_superuser(self, email, password=None):
-        user = self.create_user(email=email, password=password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
-        return user
 
+class CustomUser(AbstractBaseUser, PermissionsMixin):
 
-class CustomUser(AbstractUser):
-
-    # utilisation de l'email comme identifiant à la place du username par défaut dans AbstractUser
+    username = models.CharField(max_length=150, unique=True, blank=False)
     # email = models.EmailField(max_length=150, unique=True, blank=False)
-
-    # username = None
-    # USERNAME_FIELD = "email"
-    # REQUIRED_FIELDS = []
-
-    # age minimal RGPD : 15 ans // plutot mettre un champ j'ai plus de 15 ans OUI/NON ?
-    age = models.PositiveIntegerField(validators=[MinValueValidator(15), MaxValueValidator(120)])
-
-    can_be_contacted = models.BooleanField(blank=False)  # default=False
-    can_data_be_shared = models.BooleanField(blank=False)  # default=False
-
-    # nickname = models.CharField(max_length=80, unique=True, blank=False, verbose_name='Pseudo')
-    # profil_photo = models.ImageField(verbose_name='Photo de profil')
-
+    age = models.PositiveIntegerField(validators=[MinValueValidator(15), MaxValueValidator(120)],
+                                      blank=True,
+                                      null=True)
+    can_be_contacted = models.BooleanField(blank=False, default=False)
+    can_data_be_shared = models.BooleanField(blank=False, default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # last_login
     objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
--- src/sd_support/settings.py
+++ src/sd_support/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
+    'sd_auth',
     'sd_api',
 ]
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
@@ -123,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'sd_auth.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
