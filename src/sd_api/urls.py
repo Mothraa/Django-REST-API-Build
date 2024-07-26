a72556c0d3b408204df112926da4cb4f2512e953
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-07-26 22:42:02 +0200

    add project router

--- src/sd_api/urls.py
+++ src/sd_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomUserViewSet
+from .views import CustomUserViewSet, ProjectViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 router = DefaultRouter()
 router.register('users', CustomUserViewSet, basename='users')
+router.register('projects', ProjectViewSet, basename='projects')
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
