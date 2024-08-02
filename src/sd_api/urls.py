ae5102e276c257ddedd1ed73f32037a3506af60e
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-08-02 16:05:40 +0200

    change contributors routes

--- src/sd_api/urls.py
+++ src/sd_api/urls.py
@@ -14,10 +14,12 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(router.urls)),
-    path('api/projects/<int:project_id>/contributors/<int:user_id>/',
-         ContributorViewSet.as_view({'post': 'create', 'delete': 'destroy'}),
-         name='project-contributor-manage'),
+
     path('api/projects/<int:project_id>/contributors/',
-         ContributorViewSet.as_view({'get': 'list_contributors'}),
+         ContributorViewSet.as_view({'post': 'create', 'get': 'list_contributors'}),
          name='project-contributors-list'),
+    path('api/projects/<int:project_id>/contributors/<int:user_id>/',
+         ContributorViewSet.as_view({'delete': 'destroy'}),
+         name='project-contributor-manage'),
+    path('api/users/<int:user_id>/projects/', ContributorViewSet.as_view({'get': 'user_projects'}), name='user-projects'),
 ]
