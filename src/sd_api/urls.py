c686d8eec973682951efb2a02d51406892b1c37b
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-08-02 22:55:48 +0200

    add issues and comments router paths

--- src/sd_api/urls.py
+++ src/sd_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomUserViewSet, ProjectViewSet, ContributorViewSet
+from .views import CustomUserViewSet, ProjectViewSet, ContributorViewSet, IssueViewSet, CommentViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -8,18 +8,22 @@ router = DefaultRouter()
 router.register('users', CustomUserViewSet, basename='users')
 router.register('projects', ProjectViewSet, basename='projects')
 router.register('contributors', ContributorViewSet, basename='contributors')
+router.register('issues', IssueViewSet, basename='issue')
+router.register('comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(router.urls)),
-
-    path('api/projects/<int:project_id>/contributors/',
-         ContributorViewSet.as_view({'post': 'create', 'get': 'list_contributors'}),
-         name='project-contributors-list'),
-    path('api/projects/<int:project_id>/contributors/<int:user_id>/',
-         ContributorViewSet.as_view({'delete': 'destroy'}),
-         name='project-contributor-manage'),
-    path('api/users/<int:user_id>/projects/', ContributorViewSet.as_view({'get': 'user_projects'}), name='user-projects'),
+     path('api-auth/', include('rest_framework.urls')),
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+     path('api/', include(router.urls)),
+     # spécifique contributors
+     path('api/projects/<int:project_id>/contributors/',
+          ContributorViewSet.as_view({'post': 'create', 'get': 'list_contributors'}),
+          name='project-contributors-list'),
+     path('api/projects/<int:project_id>/contributors/<int:user_id>/',
+          ContributorViewSet.as_view({'delete': 'destroy'}),
+          name='project-contributor-manage'),
+     path('api/users/<int:user_id>/projects/',
+          ContributorViewSet.as_view({'get': 'user_projects'}),
+          name='user-projects'),
 ]
